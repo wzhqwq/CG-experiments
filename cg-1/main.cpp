a984274c4d2742da248d5f49a493977c2a996ca0
wzhqwq <wzhii345@outlook.com>
2023-03-13 19:58:15 +0800

    Initial Commit

---
+++ cg-1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cg-1
+//
+//  Created by 王子涵 on 2023/3/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
